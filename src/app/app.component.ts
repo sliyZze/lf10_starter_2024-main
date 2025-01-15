f7ed64520a3b21e0ffbfa10397ac12ad2c21cd3e
Tosoeke <156794061+Tosoeke@users.noreply.github.com>
2025-01-15 17:14:38 +0100

    keycloak

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
 
 @Component({
-    selector: 'app-root',
-    imports: [CommonModule, EmployeeListComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [CommonModule, EmployeeListComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lf10StarterNew';
