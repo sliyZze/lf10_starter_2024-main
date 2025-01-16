f159b2d8c33111f43408ddad9913e1c8707b86de
„Shawkat <shawkat1@schule.bremen.de>
2025-01-16 09:14:34 +0100

    updated

--- src/app/components/employee/employee-table/employee-table.component.ts
+++ src/app/components/employee/employee-table/employee-table.component.ts
@@ -38,4 +38,41 @@ export class EmployeeTableComponent {
   validatePageInput() {
     return this.isValid = !isNaN(Number(this.page)) && this.page.trim() !== '' || this.page.length == 0;
   }
+
+  onAddClick (){
+
+  }
+
+  employees = [
+    {
+      id: 1,
+      name: 'Mark',
+      surname: 'Otto',
+      address: 'Angularstraße 12',
+      city: 'Berlin',
+      postcode: '10115',
+      phone: '030-1234567',
+      qualifications: ['Ingenieur', 'Qualifikation2', 'Qualifikation3']
+    },
+    {
+      id: 2,
+      name: 'Jacob',
+      surname: 'Thornton',
+      address: 'Hauptstraße 5',
+      city: 'Hamburg',
+      postcode: '20095',
+      phone: '040-9876543',
+      qualifications: ['Softwareentwickler', 'Qualifikation2', 'Qualifikation3']
+    },
+    {
+      id: 3,
+      name: 'Sophie',
+      surname: 'Meier',
+      address: 'Ringstraße 8',
+      city: 'München',
+      postcode: '80331',
+      phone: '089-7654321',
+      qualifications: ['Architektin', 'Qualifikation2', 'Qualifikation3']
+    }
+  ];
 }
