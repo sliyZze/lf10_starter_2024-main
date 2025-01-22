f7a5d2349ca708435b6dd59054d15dba84edd50b
Tosoeke <156794061+Tosoeke@users.noreply.github.com>
2025-01-22 09:29:42 +0100

    Logout keycloak implementation

--- src/app/components/header/main-header/main-header.component.ts
+++ src/app/components/header/main-header/main-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NavigationService} from "../../services/NavigationService";
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-main-header',
@@ -11,9 +12,12 @@ import {NavigationService} from "../../services/NavigationService";
 })
 export class MainHeaderComponent {
 
-  constructor(private navigationService: NavigationService) {}
+  constructor(private navigationService: NavigationService, private keycloakService: KeycloakService) {}
 
   onLogoutClick() {
     this.navigationService.redirectToLogin()
+  }
+  logout(): void {
+    this.keycloakService.logout('http://localhost:4200');
   }
 }
